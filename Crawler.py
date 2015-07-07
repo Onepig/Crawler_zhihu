23599bb588f679625f4044391453a8d98d1bdfa2
Onepig <zhuyp1990@gmail.com>
2015-07-07 19:08:20 +0800

    debug3
    add "\n" int txt file for each question

--- Crawler.py
+++ Crawler.py
@@ -1207,5 +1207,6 @@ def question_titles_in_page(topic_url,page_num,name):
     for question in questions_list:
         question_url="http://www.zhihu.com"+question["href"]
         f.write(Question(question_url).get_title())
+        f.write('\n')
     f.close()
     print "Page %d has been downloaded..."%page_num
